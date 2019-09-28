e61ea542f3be3afd393c6fb1f4f8c4b293b8ba4b
Lea Rosema <terabaud@gmail.com>
2019-09-28 22:10:41 +0200

    fix: prime factors

--- src/prime-factors/prime-factors.mjs
+++ src/prime-factors/prime-factors.mjs
@@ -9,16 +9,19 @@ import { range } from "../range/range.mjs";
 export function primeFactors(num) {
   num = num | 0;
   const result = [];
-  const factors = [2, ...range(3, ((num / 2)|0) + 1, 2)];
+  const factors = [2, ...range(3, (Math.sqrt(num)|0) + 1, 2)];
   for (let i = 0; i < factors.length; i++) {
     const factor = factors[i];
     while ((num % factor) === 0) {
       num = (num / factor)|0;
       result.push(factor);
     }
-    if (i > num) {
+    if (factor > num) {
       break;
     }
+  }
+  if (num > 1) {
+    result.push(num);
   }
   return result;
 }
--- src/prime-factors/prime-factors.test.mjs
+++ src/prime-factors/prime-factors.test.mjs
@@ -1,10 +1,12 @@
 import { test } from "../test-framework/test.mjs";
-import { primeFactors } from "./prime-factors.mjs";
 import { arrayEquals } from "../test-framework/array-utils.mjs";
+import { primeFactors } from "./prime-factors.mjs";
 
 test('Test prime factorization', (assert) => {
   assert(arrayEquals([], primeFactors(1)), 'primeFactors(1) should return an empty array.');
   assert(arrayEquals([2], primeFactors(2)), 'The prime factor of 2 is 2.');
+  assert(arrayEquals([3], primeFactors(3)), 'The prime factor of 3 is 3.');
   assert(arrayEquals([2, 2], primeFactors(4)), 'The prime factors of 4 are 2*2.');
   assert(arrayEquals([2, 2, 2, 5, 5], primeFactors(200)), 'The prime factors of 200 are 2³*5².');
+  assert(arrayEquals([3, 5, 823], primeFactors(12345)), 'The prime factors of 12345 are 3, 5, 823.');
 });
