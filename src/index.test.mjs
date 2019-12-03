13e7d54dc41b6a6f7e10a9952c729be50a2d77bd
Lea Rosema <terabaud@gmail.com>
2019-12-03 09:04:12 +0100

    add describe around complex number test

--- src/index.test.mjs
+++ src/index.test.mjs
@@ -1,7 +1,7 @@
 import './bit-buffer/bit-buffer.test.mjs';
-import './complex-number/complex-number.test.mjs'
 import './gcd/gcd.test.mjs';
 import './range/range.test.mjs';
 import './prime-factors/prime-factors.test.mjs';
 import './prime-sieve/prime-sieve.test.mjs';
+import './complex-number/complex-number.test.mjs';
 import './math3d/math3d.test.mjs';
